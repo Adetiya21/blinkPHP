987ec88f53672eec1c8f13a42c19e37765887b3a
Adetiya Burhasan <adetiyaburhasanp45@gmail.com>
2019-10-16 16:33:53 +0700

    data edited-2

--- index.php
+++ index.php
@@ -146,9 +146,21 @@
 	</section>
 
 <!-- footer -->
-<?php 
-	include("/pages/layout/footer.php");
-?>
+	<footer>
+		<div class="container-bar">
+			<hr class="footer">
+			<div class="">
+				<div class="col-12">
+					<div class="col-6" align="center">
+						<img src="/assets/img/footer/footer.png" alt="Supported" class="ade">
+					</div>
+					<div class="col-6">
+						<p class="right">Copyright © 2018 AdeCreative21<br>All Rights Reserved</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
 
     <!-- <script src="/assets/js/jquery-3.3.1.min.js"></script> -->
     <script type="text/javascript">
